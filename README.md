623b8d13a85b5811f6de1c1bed668700f8f53f61
Sebastian Rager <mail@interpretor.net>
2017-05-29 21:33:09 +0200

    Add the chat package

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # zyre-chat
+
 Simple chat based on zyre.js
+
+## Installation
+
+```bash
+npm install zyre-chat
+```
+
+## Usage
+
+```js
+const chat = require('zyre-chat');
+
+chat.start();
+```
---
+++ index.js
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) 2017 Sebastian Rager
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+const readline = require('readline');
+const zyre = require('zyre.js');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: '> ',
+});
+
+function clearStdin() {
+  readline.clearLine(process.stdin, 0);
+  readline.cursorTo(process.stdin, 0);
+}
+
+class Chat {
+  static start() {
+    rl.question('Enter your name: ', (answer) => {
+      const z1 = zyre.new({ name: answer });
+
+      z1.on('connect', (id, name) => {
+        clearStdin();
+        console.log(`${name} has joined the chat`);
+        rl.prompt(true);
+      });
+
+      z1.on('disconnect', (id, name) => {
+        clearStdin();
+        console.log(`${name} has left the chat`);
+        rl.prompt(true);
+      });
+
+      z1.on('evasive', (id, name) => {
+        clearStdin();
+        console.log(`${name} not responding`);
+        rl.prompt(true);
+      });
+
+      z1.on('expired', (id, name) => {
+        clearStdin();
+        console.log(`${name} timed out`);
+        rl.prompt(true);
+      });
+
+      z1.on('message', (id, name, message) => {
+        clearStdin();
+        console.log(`<${name}> ${message}`);
+        rl.prompt(true);
+      });
+
+      z1.start().then(() => {
+        z1.join('CHAT');
+      });
+
+      rl.on('SIGINT', () => {
+        z1.stop().then(() => {
+          clearStdin();
+          process.exit(0);
+        });
+      });
+
+      rl.prompt();
+      rl.on('line', (input) => {
+        switch (input) {
+          case '/list': {
+            const peers = z1.getPeers();
+            for (const i in peers) {
+              if ({}.hasOwnProperty.call(peers, i)) {
+                console.log(`- ${peers[i].name}`);
+              }
+            }
+            break;
+          }
+
+          case '/help':
+            console.log('Type "/list" to show all connected peers');
+            break;
+
+          default:
+            z1.shout('CHAT', input);
+        }
+        rl.prompt();
+      });
+    });
+  }
+}
+
+module.exports = Chat;

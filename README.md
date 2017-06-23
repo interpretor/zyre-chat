9f36599bf8cb602ebc4bbe03996ee299c83c3998
Sebastian Rager <mail@interpretor.net>
2017-06-23 19:36:17 +0200

    Refactor the chat and add additional CLI functionality

--- README.md
+++ README.md
@@ -10,8 +10,22 @@ npm install -g zyre-chat
 
 ## Usage
 
-Start the CLI chat with the following command (arguments are optional):
+```
+Usage: zyre-chat [options]
 
-```bash
-zyre-chat --name=Foo --iface=eth0
+Options:
+  -n <name>  --name=<name>       set the name of the chat client
+  -i <iface> --iface=<iface>     set the network interface which the chat client
+                                 should use
+  -g <text>  --greeting=<text>   set a greeting message that will be sent to
+                                 every new connected client
+  -e <value> --evasive=<value>   set the timeout value in ms at which remote
+                                 clients will be marked as not responsive and
+                                 at which the chat client will force to
+                                 reconnect not responding clients
+  -E <value> --expired=<value>   set the timeout value in ms at which remote
+                                 clients will be disconnected and removed
+  -b <value> --binterval=<value> set the interval in ms of the UDP broadcast
+                                 beacon which is responsible for client
+                                 discovery and heartbeating
 ```
--- bin/start.js
+++ bin/start.js
@@ -1,13 +1,89 @@
 #! /usr/bin/env node
 const chat = require('../index');
 
+const HELPTEXT =
+`Usage: zyre-chat [options]
+
+Options:
+  -n <name>  --name=<name>       set the name of the chat client
+  -i <iface> --iface=<iface>     set the network interface which the chat client
+                                 should use
+  -g <text>  --greeting=<text>   set a greeting message that will be sent to
+                                 every new connected client
+  -e <value> --evasive=<value>   set the timeout value in ms at which remote
+                                 clients will be marked as not responsive and
+                                 at which the chat client will force to
+                                 reconnect not responding clients
+  -E <value> --expired=<value>   set the timeout value in ms at which remote
+                                 clients will be disconnected and removed
+  -b <value> --binterval=<value> set the interval in ms of the UDP broadcast
+                                 beacon which is responsible for client
+                                 discovery and heartbeating`;
+
+function printHelp() {
+  console.log(HELPTEXT);
+  process.exit(0);
+}
+
 let name;
 let iface;
+let greeting;
+let evasive;
+let expired;
+let binterval;
 
-process.argv.slice(2).forEach((e) => {
-  const arg = e.split('=');
-  if (arg[0] === '--name') name = arg[1];
-  if (arg[0] === '--iface') iface = arg[1];
+const userArgv = process.argv.slice(2);
+
+userArgv.forEach((e, i) => {
+  let cmd;
+  let arg;
+
+  if (e.length === 2 && e.substr(0, 1) === '-') {
+    cmd = e.substr(1);
+    const argvNext = userArgv[i + 1];
+    if (typeof argvNext !== 'undefined' && argvNext.length > 0) arg = argvNext;
+  } else if (e.length > 4 && e.substr(0, 2) === '--') {
+    const argvArr = e.substr(2).split('=');
+    if (argvArr[0].length > 0) cmd = argvArr[0];
+    if (argvArr.length === 2 && argvArr[1].length > 0) arg = argvArr[1];
+  } else {
+    return;
+  }
+
+  switch (cmd) {
+    case 'n':
+    case 'name':
+      name = arg;
+      break;
+
+    case 'i':
+    case 'iface':
+      iface = arg;
+      break;
+
+    case 'g':
+    case 'greeting':
+      greeting = arg;
+      break;
+
+    case 'e':
+    case 'evasive':
+      if (+arg) evasive = +arg;
+      break;
+
+    case 'E':
+    case 'expired':
+      if (+arg) expired = +arg;
+      break;
+
+    case 'b':
+    case 'binterval':
+      if (+arg) binterval = +arg;
+      break;
+
+    default:
+      printHelp();
+  }
 });
 
-chat.start(name, iface);
+chat.start(name, iface, greeting, evasive, expired, binterval);

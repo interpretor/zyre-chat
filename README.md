3bed37c92c9be2b81decd904b247ec6552e76d10
Sebastian Rager <mail@interpretor.net>
2018-05-09 12:39:33 +0200

    Update description

--- README.md
+++ README.md
@@ -14,18 +14,15 @@ npm install -g zyre-chat
 Usage: zyre-chat [options]
 
 Options:
-  -n <name>,  --name=<name>       set the name of the chat client
-  -i <iface>, --iface=<iface>     set the network interface which the chat
-                                  client should use
-  -g <text>,  --greeting=<text>   set a greeting message that will be sent to
-                                  every new connected client
-  -e <value>, --evasive=<value>   set the timeout value in ms at which remote
-                                  clients will be marked as not responsive and
-                                  at which the chat client will force to
-                                  reconnect not responding clients
-  -E <value>, --expired=<value>   set the timeout value in ms at which remote
-                                  clients will be disconnected and removed
-  -b <value>, --binterval=<value> set the interval in ms of the UDP broadcast
-                                  beacon which is responsible for client
-                                  discovery and heartbeating
+
+  -v, --version            output the version number
+  -n, --name <name>        set the name of the chat client
+  -i, --iface <iface>      set the network interface which the chat client should use
+  -g, --greeting <text>    set a greeting message that will be sent to every new connected client
+  -e, --evasive <value>    set the timeout value in ms at which remote clients will be marked as not responsive and at which the chat client will force to reconnect not responding clients
+  -E, --expired <value>    set the timeout value in ms at which remote clients will be disconnected and removed
+  -p, --port <port>        set the TCP port for incoming messages, will be incremented if already in use
+  -P, --bport <port>       set the UDP port of the broadcast beacon which is responsible for client discovery and heartbeating
+  -b, --binterval <value>  set the interval in ms of the UDP broadcast beacon which is responsible for client discovery and heartbeating
+  -h, --help               output usage information
 ```
--- bin/start.js
+++ bin/start.js
@@ -15,14 +15,14 @@ const version = require('../package.json').version;
 
 program
   .version(version, '-v, --version')
-  .option('-n, --name <name>', 'Set the name of the chat client')
-  .option('-i, --iface <iface>', 'Set the network interface which the chat client should use')
-  .option('-g, --greeting <text>', 'Set a greeting message that will be sent to every new connected client')
-  .option('-e, --evasive <value>', 'Set the timeout value in ms at which remote clients will be marked as not responsive and at which the chat client will force to reconnect not responding clients', parseInt)
-  .option('-E, --expired <value>', 'Set the timeout value in ms at which remote clients will be disconnected and removed', parseInt)
-  .option('-p, --port <port>', 'Set the TCP port for incoming messages, will be incremented if already in use', parseInt)
-  .option('-P, --bport <port>', 'Set the UDP port of the broadcast beacon which is responsible for client discovery and heartbeating', parseInt)
-  .option('-b, --binterval <value>', 'Set the interval in ms of the UDP broadcast beacon which is responsible for client discovery and heartbeating', parseInt)
+  .option('-n, --name <name>', 'set the name of the chat client')
+  .option('-i, --iface <iface>', 'set the network interface which the chat client should use')
+  .option('-g, --greeting <text>', 'set a greeting message that will be sent to every new connected client')
+  .option('-e, --evasive <value>', 'set the timeout value in ms at which remote clients will be marked as not responsive and at which the chat client will force to reconnect not responding clients', parseInt)
+  .option('-E, --expired <value>', 'set the timeout value in ms at which remote clients will be disconnected and removed', parseInt)
+  .option('-p, --port <port>', 'set the TCP port for incoming messages, will be incremented if already in use', parseInt)
+  .option('-P, --bport <port>', 'set the UDP port of the broadcast beacon which is responsible for client discovery and heartbeating', parseInt)
+  .option('-b, --binterval <value>', 'set the interval in ms of the UDP broadcast beacon which is responsible for client discovery and heartbeating', parseInt)
   .parse(process.argv);
 
 chat.start({
